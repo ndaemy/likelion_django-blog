beb49d8e03151ef7045645a4922205104447f381
yuyaebean <yuyaebean@gmail.com>
2020-07-18 23:58:45 +0900

    [4.5] Template inherit

--- likelion_blog_project/settings.py
+++ likelion_blog_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio.apps.PortfolioConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'likelion_blog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ 'likelion_blog_project/templates '],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
